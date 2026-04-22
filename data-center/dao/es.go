6bd2a38b20644b9f9535828396a6426ba629faa3
9527 <9527@gmail.com>
2026-04-22 11:22:51 +0800

    first commit

---
+++ data-center/dao/es.go
@@ -0,0 +1,186 @@
+package dao
+
+import (
+	"app/entity"
+	"context"
+	"crypto/md5"
+	"encoding/json"
+	"fmt"
+	"micro_service/services"
+	"strconv"
+
+	"github.com/olivere/elastic/v7"
+	"go.uber.org/zap"
+)
+
+type ESDao struct {
+	es *elastic.Client
+}
+
+func NewESDao(client *elastic.Client) *ESDao {
+	return &ESDao{es: client}
+}
+
+func (esDao *ESDao) BulkRecordsSave(data []*entity.CacheRecordsReq) error {
+	bulkService := esDao.es.Bulk()
+	records := make([]elastic.BulkableRequest, 0)
+	for _, req := range data {
+		hashStr := fmt.Sprintf("%d|%d|%s", req.AgentId, req.UserId, req.RoundID)
+		req.Hash = fmt.Sprintf("%x", md5.Sum([]byte(hashStr)))
+		records = append(records, elastic.NewBulkIndexRequest().Index("pp_gp_settlement").Id(req.Hash).Doc(req))
+	}
+	bulkService.Add(records...)
+	_, err := bulkService.Do(context.Background())
+	if err != nil {
+		zap.L().Error("BulkRecordsSave,批量插入数据失败", zap.Any("err", err), zap.Any("data", data))
+	}
+	return nil
+}
+
+func (esDao *ESDao) BulkBillsSave(data []*entity.CacheBillsReq) error {
+	bulkService := esDao.es.Bulk()
+	records := make([]elastic.BulkableRequest, 0)
+	for _, req := range data {
+		records = append(records, elastic.NewBulkIndexRequest().Index("pp_gp_flowing_water").Doc(req))
+	}
+	bulkService.Add(records...)
+	_, err := bulkService.Do(context.Background())
+	if err != nil {
+		zap.L().Error("BulkBillsSave,批量插入数据失败", zap.Any("err", err), zap.Any("data", data))
+	}
+	return nil
+}
+
+func (esDao *ESDao) GetRecords(userId int64, symbol, hash, currency string) []*services.RecordItem {
+	querys := make([]elastic.Query, 0)
+	if hash != "" {
+		querys = append(querys, elastic.NewTermQuery("hash", hash))
+	} else {
+		querys = append(querys, elastic.NewTermQuery("userId", userId))
+		querys = append(querys, elastic.NewTermQuery("symbol", symbol))
+		querys = append(querys, elastic.NewTermQuery("complete", true))
+	}
+	if currency != "" {
+		querys = append(querys, elastic.NewMatchPhraseQuery("currency", currency))
+	}
+	boolQuery := elastic.NewBoolQuery().Must(querys...)
+	var sorce []string = nil
+	if hash == "" {
+		sorce = []string{"userId", "agentId", "bet", "currency", "currencySymbol", "base_bet", "win", "rtp", "playedDate", "roundID", "symbol", "hash", "balance", "balance_cash", "balance_bonus", "isTourist"}
+	} else {
+		sorce = []string{"init", "log"}
+	}
+	includeFields := elastic.NewFetchSourceContext(true).Include(sorce...)
+	resp, _ := esDao.es.Search().Index("pp_gp_settlement").FetchSourceContext(includeFields).
+		Query(boolQuery).
+		Pretty(true).
+		Size(100).
+		Sort("playedDate", false).
+		Do(context.Background())
+	records := make([]*services.RecordItem, 0)
+	for _, v := range resp.Hits.Hits {
+		b, _ := v.Source.MarshalJSON()
+		r := &services.RecordItem{}
+		json.Unmarshal(b, r)
+		records = append(records, r)
+	}
+	return records
+}
+
+func (esDao *ESDao) GetRecordsByRoundId(roundId string) []*services.RecordItem {
+	querys := make([]elastic.Query, 0)
+	rid, _ := strconv.ParseInt(roundId, 10, 64)
+	querys = append(querys, elastic.NewTermQuery("roundID", rid))
+	// querys = append(querys, elastic.NewTermQuery("symbol", symbol))
+	sorce := []string{"currency", "currencySymbol", "symbol", "log"}
+	includeFields := elastic.NewFetchSourceContext(true).Include(sorce...)
+	boolQuery := elastic.NewBoolQuery().Must(querys...)
+	resp, _ := esDao.es.Search().Index("pp_gp_settlement").FetchSourceContext(includeFields).
+		Query(boolQuery).
+		Pretty(true).
+		Size(100).
+		Sort("playedDate", false).
+		Do(context.Background())
+	records := make([]*services.RecordItem, 0)
+	for _, v := range resp.Hits.Hits {
+		b, _ := v.Source.MarshalJSON()
+		r := &services.RecordItem{}
+		json.Unmarshal(b, r)
+		records = append(records, r)
+	}
+	return records
+}
+
+func (esDao *ESDao) GetRecordsReplayDataByRoundId(roundId string) []*services.RecordItem {
+	querys := make([]elastic.Query, 0)
+	rid, _ := strconv.ParseInt(roundId, 10, 64)
+	querys = append(querys, elastic.NewTermQuery("roundID", rid))
+	boolQuery := elastic.NewBoolQuery().Must(querys...)
+	resp, err := esDao.es.Search().Index("pp_gp_settlement").
+		Query(boolQuery).
+		Pretty(true).
+		Size(100).
+		Sort("playedDate", false).
+		Do(context.Background())
+	if err != nil {
+		return nil
+	}
+	records := make([]*services.RecordItem, 0)
+	for _, v := range resp.Hits.Hits {
+		b, _ := v.Source.MarshalJSON()
+		r := &services.RecordItem{}
+		json.Unmarshal(b, r)
+		records = append(records, r)
+	}
+	return records
+}
+
+func (esDao *ESDao) GetRecordsReplayDataByToken(token string) []*services.RecordItem {
+	querys := make([]elastic.Query, 0)
+	querys = append(querys, elastic.NewTermQuery("hash", token))
+	boolQuery := elastic.NewBoolQuery().Must(querys...)
+	resp, err := esDao.es.Search().Index("pp_gp_settlement").
+		Query(boolQuery).
+		Pretty(true).
+		Size(100).
+		Sort("playedDate", false).
+		Do(context.Background())
+	records := make([]*services.RecordItem, 0)
+	if err != nil {
+		return nil
+	}
+	for _, v := range resp.Hits.Hits {
+		b, _ := v.Source.MarshalJSON()
+		r := &services.RecordItem{}
+		json.Unmarshal(b, r)
+		records = append(records, r)
+	}
+	return records
+}
+
+func (esDao *ESDao) GetRtpGreaterThan10(symbol string, userId int64) []*services.RecordItem {
+	querys := make([]elastic.Query, 0)
+	sorce := []string{"userId", "agentId", "bet", "currency", "currencySymbol", "base_bet", "win", "rtp", "playedDate", "roundID", "symbol", "hash", "balance", "balance_cash", "balance_bonus"}
+	includeFields := elastic.NewFetchSourceContext(true).Include(sorce...)
+	querys = append(querys, elastic.NewTermQuery("userId", userId))
+	querys = append(querys, elastic.NewRangeQuery("rtp").Gte(10))
+	querys = append(querys, elastic.NewTermQuery("symbol", symbol))
+	boolQuery := elastic.NewBoolQuery().Must(querys...)
+	resp, err := esDao.es.Search().Index("pp_gp_settlement").FetchSourceContext(includeFields).
+		Query(boolQuery).
+		Pretty(true).
+		Size(100).
+		Sort("playedDate", false).
+		Do(context.Background())
+	if err != nil {
+		return nil
+	}
+	records := make([]*services.RecordItem, 0)
+	for _, v := range resp.Hits.Hits {
+		b, _ := v.Source.MarshalJSON()
+		r := &services.RecordItem{}
+		json.Unmarshal(b, r)
+		records = append(records, r)
+	}
+	return records
+}
---
+++ data-center/rpc/service.go
@@ -0,0 +1,179 @@
+package rpc
+
+import (
+	"app/entity"
+	"context"
+	"data-center/dao"
+	"micro_service/services"
+	"time"
+
+	"github.com/olivere/elastic/v7"
+	"github.com/redis/go-redis/v9"
+	"go.uber.org/zap"
+	"gorm.io/gorm"
+)
+
+type DataCenterService struct {
+	services.UnimplementedDataCenterServiceServer
+	db  *dao.DBDao
+	rds *dao.RedisDao
+	es  *dao.ESDao
+
+	RecordChan chan *entity.CacheRecordsReq
+}
+
+func NewDataCenterService(db, mdb *gorm.DB, es *elastic.Client) *DataCenterService {
+	tmp := &DataCenterService{
+		db:         dao.NewDBDao(db, mdb),
+		rds:        dao.RedisIns(),
+		es:         dao.NewESDao(es),
+		RecordChan: make(chan *entity.CacheRecordsReq, 10240),
+	}
+
+	tmp.syncRecords()
+	return tmp
+}
+
+// 注单入库
+func (dc *DataCenterService) syncRecords() {
+	go func() {
+		defer func() {
+			if e := recover(); e != nil {
+				zap.L().Error("syncRecords,数据落地协程panic", zap.Any("recover", e))
+			}
+		}()
+		data := make([]*entity.CacheRecordsReq, 0, 16)
+		//十秒写一次
+		t := time.NewTicker(10 * time.Second)
+		for {
+			select {
+			case <-t.C:
+				if len(data) > 0 {
+					dc.es.BulkRecordsSave(data)
+					ids := make([]string, 0, 64)
+					for _, v := range data {
+						ids = append(ids, v.RoundID)
+					}
+					zap.L().Debug("批量写入注单信息", zap.Any("roundIds", ids))
+					data = make([]*entity.CacheRecordsReq, 0, 16)
+				}
+			case req := <-dc.RecordChan:
+				data = append(data, req)
+				if len(data) >= 8 {
+					dc.es.BulkRecordsSave(data)
+					ids := make([]string, 0, 64)
+					for _, v := range data {
+						ids = append(ids, v.RoundID)
+					}
+					zap.L().Debug("批量写入注单信息", zap.Any("roundIds", ids))
+					data = make([]*entity.CacheRecordsReq, 0, 16)
+				}
+			}
+		}
+	}()
+}
+
+func (d *DataCenterService) GetPlayer(ctx context.Context, req *services.GetPlayerReq) (resp *services.GetPlayerResp, err error) {
+	p, err := d.rds.GetPlayer(req.GetPlayerId(), req.Factory)
+	if err != nil {
+		return nil, err
+	}
+	resp = &services.GetPlayerResp{}
+	if p != nil {
+		resp.HumanPlayer = p
+		return resp, nil
+	}
+	// 在redis中没有找到玩家信息，从db中加载
+	player, err := d.db.GetPlayer(ctx, req.PlayerId)
+	if err != nil {
+		return nil, err
+	}
+	p = ConvertUserEntityToHumanPlayer(player)
+	if err := d.rds.SetPlayer(p, false); err != nil {
+		zap.L().Warn("向redis写入玩家信息缓存失败，下一次获取该玩家信息时还将从数据库中读取", zap.Uint32("player_id", p.Id))
+	}
+	resp.HumanPlayer = p
+	zap.L().Debug("get player", zap.Any("resp", resp))
+	return resp, nil
+}
+
+func (d *DataCenterService) UpdatePlayerAvatarAndGender(ctx context.Context, req *services.UpdatePlayerAvatarAndGenderReq) (resp *services.UpdatePlayerAvatarAndGenderResp, err error) {
+	err = d.rds.UpdatePlayerAvatarAndGender(req.PlayerId, req.Avatar, req.Name, req.Gender)
+	if err == redis.Nil {
+		// 在redis中没有找到玩家信息，从db中加载
+		player, err := d.db.GetPlayer(ctx, req.PlayerId)
+		if err != nil {
+			zap.L().Error("从数据库加载玩家信息失败", zap.Stringer("req", req), zap.Error(err))
+			return nil, err
+		}
+		if err := d.rds.SetPlayer(ConvertUserEntityToHumanPlayer(player), true); err != nil {
+			zap.L().Error("向redis写入玩家信息缓存失败", zap.Stringer("req", req), zap.Error(err))
+			return nil, err
+		}
+	} else if err != nil {
+		zap.L().Error("更新玩家头像或性别失败", zap.Stringer("req", req), zap.Error(err))
+		return nil, err
+	}
+	return &services.UpdatePlayerAvatarAndGenderResp{}, nil
+}
+
+func (d *DataCenterService) GetValue(_ context.Context, req *services.GetValueReq) (resp *services.GetValueResp, err error) {
+	resp = &services.GetValueResp{Code: services.ErrorCode_OK}
+	res, err := d.rds.Get(req.Key, req.TimeOut)
+	if err != nil && err != redis.Nil {
+		resp.Code = services.ErrorCode_SYSTEM_ERROR
+		return resp, err
+	}
+	resp.Value = res
+	return resp, nil
+}
+
+func (d *DataCenterService) SetValue(_ context.Context, req *services.SetValueReq) (resp *services.SetValueResp, err error) {
+	resp = &services.SetValueResp{Code: services.ErrorCode_OK}
+	if err := d.rds.Set(req.Key, req.Value, req.TimeOut); err != nil {
+		resp.Code = services.ErrorCode_SYSTEM_ERROR
+		return nil, err
+	}
+	return resp, nil
+}
+
+// 获取注单信息
+func (d *DataCenterService) GetRecords(ctx context.Context, req *services.GetRecordsReq) (resp *services.GetRecordsResp, err error) {
+	resp = &services.GetRecordsResp{}
+	resp.Code = services.ErrorCode_OK
+	resp.Data = d.es.GetRecords(req.UserId, req.Symbol, req.Hash, "")
+	return resp, nil
+}
+
+// 玩家锁 加锁
+func (d *DataCenterService) UserLock(ctx context.Context, req *services.UserLockReq) (resp *services.UserLockResp, err error) {
+	resp = &services.UserLockResp{}
+	resp.Result = d.rds.UserLock(req.UserId)
+	return resp, nil
+}
+
+// 玩家锁 解锁
+func (d *DataCenterService) UserUnLock(ctx context.Context, req *services.UserUnLockReq) (resp *services.UserUnLockResp, err error) {
+	resp = &services.UserUnLockResp{}
+	resp.Result = d.rds.UserUnLock(req.UserId, req.Token)
+	return resp, nil
+}
+
+// 获取游戏列表
+func (d *DataCenterService) GetGameList(ctx context.Context, req *services.GetGameListReq) (resp *services.GetGameListResp, err error) {
+	resp = d.db.GetGameList()
+	// zap.L().Debug("游戏列表", zap.Any("data", resp), zap.Any("count", len(resp.All)))
+	return resp, nil
+}
+
+func (d *DataCenterService) GetSesson(ctx context.Context, req *services.GetSessionReq) (resp *services.GetSessionResp, err error) {
+	resp = &services.GetSessionResp{Code: services.ErrorCode_OK}
+	res, err := d.rds.Get(req.Key, req.TimeOut)
+	if err != nil && err != redis.Nil {
+		zap.L().Debug("====>getSession", zap.Any("req", req))
+		resp.Code = services.ErrorCode_SYSTEM_ERROR
+		return resp, err
+	}
+	resp.Value = res
+	return resp, nil
+}
